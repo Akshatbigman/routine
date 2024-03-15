83b2328081210bfc8ac28e1a30315fb9112f68f5
Username <darlanpacheco@proton.me>
2024-03-15 18:51:37 -0300

    First start.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./project/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-bg": "#1E1E1E",
+      },
+    },
+  },
+  plugins: [],
+};
