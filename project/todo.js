d74ecdd83e593aee46ebbade4941684f19aaff7b
Username <darlanpacheco@proton.me>
2024-03-28 15:43:50 -0300

    LocalStorage new feature and let to const variables.

--- project/todo.js
+++ project/todo.js
@@ -1,39 +1,53 @@
-let input = document.querySelector("input");
-let button = document.querySelector("#button");
-let main = document.querySelector("#todos");
+const input = document.querySelector("input");
+const button = document.querySelector("#button");
+const main = document.querySelector("#todos");
 
-let template = document.createElement("div");
+const template = document.createElement("div");
 template.setAttribute("id", "todo-item");
 template.setAttribute(
   "class",
   "p-1 my-4 hover:cursor-pointer bg-light-gray-bg hover:bg-active-gray-bg rounded-xl flex justify-between items-center overflow-hidden"
 );
 
-let checkedButton = document.createElement("input");
+const checkedButton = document.createElement("input");
 checkedButton.setAttribute(
   "class",
   "w-10 h-10 appearance-none bg-white checked:bg-green-500 hover:cursor-pointer rounded-lg"
 );
 checkedButton.setAttribute("type", "checkbox");
 
-let content = document.createElement("span");
+const content = document.createElement("span");
 content.setAttribute("class", "px-4 pointer-events-none overflow-hidden w-3/4");
 
 template.appendChild(content);
 template.appendChild(checkedButton);
 
-let taskCreator = () => {
-  if (input.value != "") {
-    let templateCopy = template.cloneNode(true);
-    main.appendChild(templateCopy);
-
-    templateCopy.children[0].innerHTML = input.value;
-
-    templateCopy.addEventListener("click", (event) => {
+const eventTodos = () => {
+  mainContent = main.querySelectorAll("#todo-item");
+  mainContent.forEach((node) => {
+    node.addEventListener("click", (event) => {
       if (event.target.id === "todo-item") {
         event.target.remove();
       }
     });
+  });
+
+  localStorage.setItem("main-content", main.innerHTML);
+};
+
+window.addEventListener("load", () => {
+  main.innerHTML = localStorage.getItem("main-content");
+  eventTodos();
+});
+
+const taskCreator = () => {
+  if (input.value != "") {
+    const templateCopy = template.cloneNode(true);
+    main.appendChild(templateCopy);
+
+    templateCopy.children[0].innerHTML = input.value;
+
+    eventTodos();
 
     input.value = "";
   }
@@ -41,9 +55,9 @@ let taskCreator = () => {
 
 button.addEventListener("click", taskCreator) ||
   input.addEventListener("keydown", function (event) {
-    // Verifica se a tecla pressionada é Enter (código 13)
     if (event.key === "Enter") {
-      // Chama a função que você deseja executar aqui
       taskCreator();
     }
   });
+
+window.addEventListener("beforeunload", eventTodos);
